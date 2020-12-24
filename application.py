2151dd525a7881ae99fa5ac5e1ae951f0f3175b4
euguroglu <e.uguroglu@yahoo.com>
2020-12-25 00:31:39 +0300

    many 2 many relationship added

--- application.py
+++ application.py
@@ -16,8 +16,11 @@ class Member(db.Model):
     password = db.Column(db.String(30))
     email = db.Column(db.String(50))
     join_date = db.Column(db.DateTime)
-
+#one to many relationship code
     orders = db.relationship('Order',backref='member',lazy='dynamic')
+#Many to many relationship code
+    courses = db.relationship('Course',secondary='user_courses',backref='member',lazy='dynamic')
+
 
     def __init__(self,username,password,email,join_date):
 
@@ -29,11 +32,21 @@ class Member(db.Model):
     def __repr__(self):
 
         return f"Member: {self.username}"
-
+#One to many relationship
 class Order(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     price = db.Column(db.Integer)
     member_id = db.Column(db.Integer,db.ForeignKey('member.id'))
+
+#Many to many relationship
+class Course(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(20))
+
+db.Table('user_courses',
+        db.Column('member_id',db.Integer,db.ForeignKey('member.id')),
+        db.Column('course_id',db.Integer,db.ForeignKey('course.id'))
+        )
 
 if __name__ == "__main__":
     app.run()
